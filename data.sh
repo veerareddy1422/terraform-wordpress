03ac84547d8e7ce145d0483771325c0f6e2d6183
veera reddy inakollu <ivreddy1422@gmail.com>
2025-05-02 10:56:05 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum install -y git
-sudo yum install -y python3-pip
-git clone https://github.com/veerareddy1422/indian-liver-patients.git
-cd Medical-Insurance/
-pip3 install -r requirements.txt
-python3 app.py
-
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo rm -rf /var/www/html/
+git clone https://github.com/veerareddy1422/food.git /var/www/html/
